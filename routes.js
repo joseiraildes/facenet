784459c1f7efe4a36d6ba5d3a8a30c1f91be27b9
joseiraildes <joseiraildesciprianoribeiro1@gmail.com>
2025-01-03 20:54:09 -0300

    feat(routes): creating a new route authentication

--- routes.js
+++ routes.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = require("./config/config.js")
 const hbs = require("express-handlebars")
 const path = require("path")
+const Ip = require('./api/ip.js')
+const MySql = require('./mysql/connection.js')
+const User = require('./models/User.js')
 
 
 app.engine("hbs", hbs.engine({ extname: "hbs" }))
@@ -12,5 +15,21 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.get("/", async(req, res)=>{
-  res.render("home")
+  const ip = await Ip()
+  const mysql = await MySql()
+  try{
+    const user = await User.findOne({
+      where: {
+        ip: ip.ip
+      }
+    })
+
+    if(user===null){
+      res.redirect("/login")
+    }else{
+      res.render("home")
+    }
+  }catch(error){
+    res.status(500).send("INTERNAL SERVER ERROR")
+  }
 })
